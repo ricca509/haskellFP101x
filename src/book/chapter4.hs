d57b9dd66d8753ac4e3e15113f084c93c6055f3f
Riccardo Coppola <ricca509@gmail.com>
2014-11-01 20:00:17 +0000

    Addes exercises for chapter 4.

--- src/book/chapter4.hs
+++ src/book/chapter4.hs
@@ -29,3 +29,42 @@ safetail_b xs | null xs = xs
 
 safetail_c [] = []
 safetail_c xs = tail xs
+
+-- 3. In a similar way to ∧, show how the logical disjunction operator ∨ can be
+-- defined in four different ways using pattern matching.
+-- No need for four ways
+
+True ∧ _ = True
+False ∧ b = b
+
+-- 4. Redefine the following version of the conjunction operator using conditional
+-- expressions rather than pattern matching:
+-- True ∧ True = True
+-- _ ∧ _ = False
+
+myAnd a =
+	\ b ->
+		if a == True && b == True
+			then
+				True
+			else
+				False
+
+-- 5. Do the same for the following version, and note the difference in the number
+-- of conditional expressions required:
+-- True ∧ b = b
+-- False ∧ _ = False
+
+myAnd2 a =
+	\ b ->
+		if a == True
+			then b
+			else False
+
+-- 6. Show how the curried function definition mult x y z = x ∗ y ∗ z can be
+-- understood in terms of lambda expressions.
+
+mult x =
+	\y ->
+		\z ->
+			x * y * z
